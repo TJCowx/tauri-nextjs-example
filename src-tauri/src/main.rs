f3e52987f05c7274e80a5843ceb06dcf1e6f9f46
Travis <tcowx21@gmail.com>
2023-02-11 14:24:20 -0500

    Added API call example

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,8 +3,14 @@
   windows_subsystem = "windows"
 )]
 
+#[tauri::command]
+fn test_call() -> Result<String, String> {
+  Ok("Hello Rust!".into())
+}
+
 fn main() {
   tauri::Builder::default()
+    .invoke_handler(tauri::generate_handler![test_call])  
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
+import { invoke } from '@tauri-apps/api/tauri';
 import { useState } from 'react';
 
 export default function Home() {
   const [apiRes, setApiRes] = useState('uncalled');
+  const [rustRes, setRustRes] = useState('uncalled');
 
+  // This will fail in a production build
   const callApi = () => {
     fetch('/api/hello', { method: 'GET' })
       .then(async (res) => {
@@ -15,12 +18,27 @@ export default function Home() {
       });
   };
 
+  // This works in both development and production builds
+  const callRust = () => {
+    invoke('test_call')
+      .then((res) => setRustRes(res as string))
+      .catch((e) => {
+        console.error(e);
+        setRustRes('Call Failed');
+      });
+  };
+
   return (
-    <main>
-      Hello world!
+    <main style={{ paddingTop: '16px' }}>
+      <h2>Hello world!</h2>
       <br />
-      <button onClick={callApi}>Test API Call</button>
+      <button onClick={callApi}>Test NextJs API Route Call</button>
       <div>Response: {apiRes}</div>
+      <br />
+      <hr />
+      <br />
+      <button onClick={callRust}>Test Rust API Call</button>
+      <div>Response: {rustRes}</div>
     </main>
   );
 }
