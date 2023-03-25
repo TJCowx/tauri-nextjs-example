704d6f964b43ce0eb0a77fc892637acf3ff6bf4b
Travis <tcowx21@gmail.com>
2023-03-25 19:50:25 -0400

    Base Setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,24 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+import createCache from '@emotion/cache';
+import { CacheProvider } from '@emotion/react';
+import { CssBaseline } from '@mui/material';
+
+import type { AppProps } from 'next/app';
+
+const muiCache = createCache({
+  key: 'mui',
+  prepend: true,
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={muiCache}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </CacheProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,49 +1,25 @@
+import { Paper, styled } from '@mui/material';
 import { invoke } from '@tauri-apps/api/tauri';
 import { useState } from 'react';
 
-export default function Home() {
-  const [apiRes, setApiRes] = useState('uncalled');
-  const [rustRes, setRustRes] = useState('uncalled');
+const Root = styled('div')(() => ({
+  height: '100%',
+  width: '100%',
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
+}));
 
-  // This will fail in a production build
-  const callApi = () => {
-    fetch('/api/hello', { method: 'GET' })
-      .then(async (res) => {
-        const parsed = await res.json();
-        setApiRes(parsed.name);
-      })
-      .catch((e) => {
-        console.log(e);
-        setApiRes('Call Failed');
-      });
-  };
+const StyledPaper = styled(Paper)(() => ({
+  width: '400px',
+  height: '200px',
+  padding: '24px',
+}));
 
-  // This works in both development and production builds
-  const callRust = (nested: boolean) => {
-    invoke(nested ? 'hello_nested' : 'test_call')
-      .then((res) => setRustRes(res as string))
-      .catch((e) => {
-        console.error(e);
-        setRustRes('Call Failed');
-      });
-  };
-
+export default function Connect() {
   return (
-    <main style={{ paddingTop: '16px' }}>
-      <h2>Hello world!</h2>
-      <br />
-      <button onClick={callApi}>Test NextJs API Route Call</button>
-      <div>Response: {apiRes}</div>
-      <br />
-      <hr />
-      <br />
-      <button onClick={() => callRust(false)}>Test Rust API Call</button>
-      <div>Response: {rustRes}</div>
-      <br />
-      <hr />
-      <br />
-      <button onClick={() => callRust(true)}>Test Nested Rust API Call</button>
-      <div>Response: {rustRes}</div>
-    </main>
+    <Root style={{ paddingTop: '16px' }}>
+      <StyledPaper>Hello World</StyledPaper>
+    </Root>
   );
 }
